03a0e7219217db991e69c62d4878c80933156acf
anand <anandpathak69@gmail.com>
2019-07-04 00:32:09 +0530

    added redis for caching and session storage

---
+++ app/plugins/redis-session/index.ts
@@ -0,0 +1,20 @@
+import { PluginType } from '../../plugin';
+const session = require('koa-session');
+// import * as passport from 'koa-passport';
+const RedisStore = require('koa-redis');
+
+export const initWebApp = function(params: PluginType) {
+    const redisConf: any = params.config.get('Redis');
+    // add redis cluster configuration
+    // const nodes = redisConf.nodes.map( (node: string ) => { return { host: node.split(':')[0], port: node.split(':')[1] }; });
+    params.app.keys = ['secret', 'key'];
+    params.app.use(session({
+        store: new RedisStore({
+            key: redisConf.key,
+            maxAge: 21600000,
+            // isRedisCluster: true,
+            // nodes: nodes,
+            url: redisConf.URL
+        })
+      }, params.app));
+};
