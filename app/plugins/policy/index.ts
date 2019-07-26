5b6a312bafca8a4375de60ae06f7143853d65449
Mihir Kumar <mkumar@machinesense.com>
2019-07-26 16:04:49 +0530

    HTTP method fetch added

--- app/plugins/policy/index.ts
+++ app/plugins/policy/index.ts
@@ -1,7 +1,6 @@
 import { PluginType } from '../../plugin';
 import { Policy } from './policy';
 
-
 export const initWebApp = function(params: PluginType) {
     params.app.policy = params.app.policy || {};
     const policy = new Policy(params);
@@ -9,4 +8,5 @@ export const initWebApp = function(params: PluginType) {
     params.app.policy['JWTAuth'] = policy.JWTAuth.bind(policy);
     params.app.policy['Authorization'] = policy.Authorization.bind(policy);
     params.app.policyInfo = policy.PolicyInfo();
+    params.app.methodList = policy.MethodList();
 };
