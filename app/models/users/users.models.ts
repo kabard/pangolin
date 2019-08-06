37ba680a294fea71db90973afb01442ee620fc8d
Mihir Kumar <mkumar@machinesense.com>
2019-08-06 11:15:55 +0530

    First time server setup feature Backend Tweak

--- app/models/users/users.models.ts
+++ app/models/users/users.models.ts
@@ -7,49 +7,43 @@ export class UsersModel extends BaseModel {
     constructor() {
         super(new Users().model);
 
-        this.verifyAdminFirstRun().then((doc: any) => {
-            console.log('\x1b[33m%s\x1b[0m', `\n\n
-            ######################################################################
-            ######################################################################
-            ##############             Admin Exists              #################
-            ##############      ${JSON.stringify(doc[0])}        #################
-            ##############           Use Any To login            #################
-            ######################################################################
-            ######################################################################
-            \n\n`);
-        }).catch((err) => {
-            if (err === 'No admin users') {
-                const doc = {
-                    username: 'ProxyDefault',
-                    roles: 'admin',
-                    password: 'MSProxy@2019'
-                };
-                this.save(doc).then((res: any) => {
-                    console.log('\x1b[33m%s\x1b[0m', `\n\n
-                    ######################################################################
-                    ######################################################################
-                    ##############         Admin Exists                  #################
-                    ##############       ${JSON.stringify(res)}          #################
-                    ##############       Use It to login first time      #################
-                    ######################################################################
-                    ######################################################################
-                    \n\n`);
-                }).catch((err) => {
-                    console.error(err);
-                });
-            }
-        });
+        this.verifyAdminFirstRun();
     }
 
     verifyAdminFirstRun() {
-        return new Promise((resolve, reject) => {
-            this._model.find({roles: 'admin'}).exec((err: any, doc: any) => {
-                if (!err && doc) {
-                    resolve(doc);
-                } else {
-                    reject(err || 'No admin users');
+        this._model.find({ roles: 'admin' }).exec((err: any, doc: any) => {
+            if (!err && doc) {
+                console.log('\x1b[33m%s\x1b[0m', `\n\n
+                    ######################################################################
+                    ######################################################################
+                    ##############             Admin Exists              #################
+                                            Username: ${JSON.stringify(doc[0].username)}
+                    ##############           Use Any To login            #################
+                    ######################################################################
+                    ######################################################################
+                    \n\n`);
+            } else {
+                if (err === 'No admin users') {
+                    const doc = {
+                        username: 'ProxyDefault',
+                        roles: 'admin',
+                        password: 'MSProxy@2019'
+                    };
+                    this.save(doc).then((res: any) => {
+                        console.log('\x1b[33m%s\x1b[0m', `\n\n
+                            ######################################################################
+                            ######################################################################
+                            ##############         Admin Exists                  #################
+                                    ${JSON.stringify(res)}
+                            ##############       Use It to login first time      #################
+                            ######################################################################
+                            ######################################################################
+                            \n\n`);
+                    }).catch((err) => {
+                        console.error(err);
+                    });
                 }
-            });
+            }
         });
     }
 
